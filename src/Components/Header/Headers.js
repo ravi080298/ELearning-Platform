f2e6f001e02816820809d097055903d1b8052c9b
ravi080298 <ravirajcjaudhary51@gmail.com>
2021-04-13 21:10:07 +0530

    commit

--- src/Components/Header/Headers.js
+++ src/Components/Header/Headers.js
@@ -10,17 +10,17 @@ const Headers = () =>{
             <div className="container">
             <div className="c1">
                 {/* <span className="cm dec">Coursera</span> */}
-                <a className="cm dec tagName">COURSERA</a>
+                <span className="cm dec tagName">COURSERA</span>
                 <Explore />
                 <input type="text"className="cm dec input1" placeholder="What do you want to learn?" name="search" />   
-                <a className="cm dec btn2 spec"><img src={src} alt="image" className="imgtag" /></a>
+                <span className="cm dec btn2 spec"><img src={src} alt="search" className="imgtag" /></span>
                 </div> 
                 <div className="c2">
-                    <a href="" className="dec">For Enterprises </a>
-                    <a href="" className="dec">For Students</a>
-                    <a href="" className="dec chnageColor">Log In</a>
+                    <span  className="dec">For Enterprises </span>
+                    <span className="dec">For Students</span>
+                    <span href="" className="dec chnageColor">Log In</span>
 
-                <button className="ml-5" className="cm btn1 ">Join for Free</button>
+                <button className="cm btn1 ">Join for Free</button>
                 {/* <button className="cm btn">Log In</button>
                 <button className="cm btn">For Students</button>
                 <button className="cm btn">For Enterprises</button> */}
--- src/Components/Section/Firstsection.js
+++ src/Components/Section/Firstsection.js
@@ -8,7 +8,7 @@ const FirstSection = () => {
                 <p className="f-w">Without</p>
                 <p className="f-w">Limits</p>
                 <p className="text-css">Take the next step in your career with a world class learning experience.</p>
-                <a className=" btn ">Join Today</a>
+                <div className=" btn ">Join Today</div>
                 <div className="sec2">
                 <img height="100px" width="50px" className="img-css" src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://images.ctfassets.net/wp1lcwdav1p1/33EmCuyKJT5MD5DOiYCcCV/993af7e2f4b789aed6193ff1978fe5d6/IN_HERO_C.png?auto=format%2Ccompress&dpr=1&w=459&h=497&q=40" alt="imgage" />
             </div>
